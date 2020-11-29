510fdd3bf6ef5f6178f9f53cfc43f9c24462d8ae
luigiVezzoso <luigi.vezzoso@gmail.com>
2020-11-30 00:10:45 +0100

    Nuovo Branch di test

--- src/main/java/cryptoTest.java
+++ src/main/java/cryptoTest.java
@@ -1,5 +1,11 @@
 public class cryptoTest {
+    private int variabile = 0;
     public static void main(String[] args) {
         System.out.println("Luigi Vezzoso");
     }
+
+    private int get(){
+        return variabile;
+    }
 }
+
